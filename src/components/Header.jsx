86572f02efc81a77f4cd57ddfe43748b4e7f39d7
Lena <lena.sterz98@gmail.com>
2023-07-22 16:03:12 +0200

    Integrate a small bit of styling for testing purpose

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,30 @@
-function Header() {
+import PropTypes from "prop-types";
+
+function Header({ text, bgColor, textColor }) {
+	const headerStyle = {
+		backgroundColor: bgColor,
+		color: textColor,
+	};
+
 	return (
-		<header>
+		<header style={headerStyle}>
 			<div className="container">
-				<h2>Feedback UI</h2>
+				<h2>{text}</h2>
 			</div>
 		</header>
 	);
 }
+
+Header.defaultProps = {
+	text: "Feedback UI",
+	bgColor: "rgba(0,0,0,0.4)",
+	textColor: "#ff6a95",
+};
+
+Header.propTypes = {
+	text: PropTypes.string,
+	bgColor: PropTypes.string,
+	textColor: PropTypes.string,
+};
 
 export default Header;
